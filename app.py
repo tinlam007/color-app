38a24ecfc4ce8ca0e71030c5bb29ef7d07453086
tinlam007 <93601379+tinlam007@users.noreply.github.com>
2021-11-19 16:12:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ color_codes = {
     "purple": "#7d3c98"
 }
 
-color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
+# color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
 
-#color = os.environ.get('APP_COLOR') or random.choice(["blue","yellow"])
+color = os.environ.get('APP_COLOR') or random.choice(["blue","yellow"])
 
 @app.route("/")
 def main():
